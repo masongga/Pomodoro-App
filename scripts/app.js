06db3af2fa2f7097c0213dd77b3645f649e70352
masongga <auckmason@gmail.com>
2025-11-10 10:28:21 -0700

    set up initial page layout

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+const setTimerBtn = document.getElementById("timerButton");
+const studyTime = document.getElementById("studyTime");
+const breakTime = document.getElementById("breakTime");
+
+let timer = 0;
+
+setTimerBtn.addEventListener("click", startTimer());
+
+function startTimer() {
+  return;
+}
